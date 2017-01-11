7093683274955b1a03ed2efb2045aa4b6a6f9c07
Murray Campbell <MurrayIRC@gmail.com>
2017-01-10 20:49:45 -0600

    CHANGELIST
    - made carrying objects affect movement / rotation speed.
    - made cam a lil faster!
    - started water droplet shader and droplet logic

---
+++ Assets/Scripts/Game/PickupObjects/WaterDroplet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDroplet : Pickupable 
+{
+	Material dropletMat;
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		dropletMat = GetComponentInChildren<MeshRenderer>().sharedMaterial;
+	}
+
+	// This gets called when we pick up the object. Pickupable controls its own rigidbody.
+    public override void OnPickup()
+    {
+        base.OnPickup();
+    }
+
+    public override void DropSelf()
+    {
+        base.DropSelf();
+    }
+}
