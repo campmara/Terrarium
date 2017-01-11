673cb8064d7a1496065dc7e108dab5585bf8b4b1
Murray Campbell <MurrayIRC@gmail.com>
2017-01-10 23:10:37 -0600

    Water Droplet Stuff
    - implemented smearing on carry of droplet

--- Assets/Scripts/Game/PickupObjects/WaterDroplet.cs
+++ Assets/Scripts/Game/PickupObjects/WaterDroplet.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class WaterDroplet : Pickupable 
 {
-	Material dropletMat;
+    [ReadOnly, SerializeField] Material dropletMat;
+
+    Vector3 position;
+    Vector3 prevPosition;
 
 	protected override void Awake()
 	{
 		base.Awake();
 
 		dropletMat = GetComponentInChildren<MeshRenderer>().sharedMaterial;
+
+        position = transform.position;
+        prevPosition = transform.position;
 	}
+
+    void Update()
+    {
+        prevPosition = position;
+        position = transform.position;
+
+        dropletMat.SetVector("_Position", position);
+        dropletMat.SetVector("_PrevPosition", prevPosition);
+    }
 
 	// This gets called when we pick up the object. Pickupable controls its own rigidbody.
     public override void OnPickup()
