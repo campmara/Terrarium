140458d69647fda225a0377bdda24ac8350a4a2f
Murray Campbell <MurrayIRC@gmail.com>
2017-01-12 16:08:31 -0600

    changelist
    - heyoooo we’re using box colliders for the pickup objects again hahah
    sorry

--- Assets/Scripts/Game/PickupObjects/PickupCollider.cs
+++ Assets/Scripts/Game/PickupObjects/PickupCollider.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(BoxCollider))]
 public class PickupCollider : MonoBehaviour 
 {
 	[ReadOnly] public Pickupable parentPickupable;
