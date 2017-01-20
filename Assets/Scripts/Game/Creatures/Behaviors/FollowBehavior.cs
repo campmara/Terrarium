ac1a4c1115e17c73d295af58562c80bdfd7960d2
Murray Campbell <MurrayIRC@gmail.com>
2017-01-20 11:59:50 -0600

    changelist
    - disc
    - transparent w/ color val surface shader
    - ring
    - pond changes
    - nothing really done yet

---
+++ Assets/Scripts/Game/Creatures/Behaviors/FollowBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBehavior : CreatureBehavior 
+{
+	public override void Enter()
+	{
+		
+	}
+
+	public override void Exit()
+	{
+
+	}
+
+	public override void Handle()
+	{
+		Debug.Log("FOLLOW: HANDLE");
+	}
+}
---
+++ Assets/Scripts/Game/Creatures/Behaviors/IdleBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleBehavior : CreatureBehavior 
+{
+	public override void Enter()
+	{
+
+	}
+
+	public override void Exit()
+	{
+
+	}
+
+	public override void Handle()
+	{
+		Debug.Log("IDLE: HANDLE");
+	}
+}
