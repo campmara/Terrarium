38ce3459b0963ddf4e2c2de638976a0c368a6ff2
mhale <mariahghale@gmail.com>
2017-02-11 11:15:48 -0600

    grass can flip down now! woop!

---
+++ Assets/Scripts/Game/Plants/GrassBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassBehaviour : PlantBehaviour 
+{
+	Animator _anim = null;
+	bool _isSquishing = false;
+
+
+	void Awake()
+	{
+		_anim = GetComponent<Animator>();
+		_anim.SetBool( "isSquishing", false );
+		//_anim.runtimeAnimatorController.
+	}
+		
+	protected override void ReactToPlayerEntrance()
+	{
+		_anim.SetBool( "isSquishing", true );
+
+	}
+
+	protected override void ReactToPlayerExit()
+	{
+		_anim.SetBool( "isSquishing", false );
+	}
+}
