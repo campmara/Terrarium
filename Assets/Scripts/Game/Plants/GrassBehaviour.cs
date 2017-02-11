d15a2e113b85d97952aef4b6460e3c25b18365b6
mhale <mariahghale@gmail.com>
2017-02-11 11:51:43 -0600

    fixing some sizes on the prefabs, changing ground plant to not have wind material (making it disappear rn))

--- Assets/Scripts/Game/Plants/GrassBehaviour.cs
+++ Assets/Scripts/Game/Plants/GrassBehaviour.cs
@@ -7,18 +7,15 @@ public class GrassBehaviour : PlantBehaviour
 	Animator _anim = null;
 	bool _isSquishing = false;
 
-
 	void Awake()
 	{
 		_anim = GetComponent<Animator>();
 		_anim.SetBool( "isSquishing", false );
-		//_anim.runtimeAnimatorController.
 	}
 		
 	protected override void ReactToPlayerEntrance()
 	{
 		_anim.SetBool( "isSquishing", true );
-
 	}
 
 	protected override void ReactToPlayerExit()
