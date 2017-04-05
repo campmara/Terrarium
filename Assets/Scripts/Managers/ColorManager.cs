593c5a5a4b3e58c72f08d3c9138b5d1804f4f1da
JohnGroot <johngroot94@gmail.com>
2017-04-05 00:51:45 -0500

    Fleshed Out ColorManager A Bit
    - Added ColorManager editor changing functionality
    - moved over stuff from Thomas' script into the ColorManager
    - Laid Groundwork for working on tree color changing stuff, isn't ready yet tho

--- Assets/Scripts/Managers/ColorManager.cs
+++ Assets/Scripts/Managers/ColorManager.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ColorManager : SingletonBehaviour<ColorManager> {
 
-	[System.Serializable]
+	[Serializable]
 	public struct EnvironmentPalette
 	{
 		public string title;
@@ -22,7 +23,7 @@ public class ColorManager : SingletonBehaviour<ColorManager> {
 		public Color skyColor;
 		public Color cloudRimColor;
 
-		//making a system for these is going to require more organization / thought.
+
 		/*
         [Header("Plant colors")]
         [Header("Class 1")]
@@ -33,33 +34,94 @@ public class ColorManager : SingletonBehaviour<ColorManager> {
         public Gradient leafyGroundPlantBulb;
         public Gradient leafyGroundPlantLeaf;
         [Header("Class 3")]
-        public Gradient twistPlant;
-        public Gradient cappPlant;
-        */
+		public Gradient twistPlant;
+		public Gradient cappPlant;
+		*/
 	}
 		
-	public enum PaletteIndex : int
-	{
-		NONE = -1,
-		DESERT,
-		CLASSIC,
-		ABZU,
-		RED_MOON
-	}
-	[SerializeField] PaletteIndex _currentPalette = PaletteIndex.ABZU;
+	public static event Action<EnvironmentPalette> ExecutePaletteChange;
 
+	[SerializeField] int _paletteIndex = 0;
+	EnvironmentPalette _activePalette;
+	public EnvironmentPalette ActivePalatte { get { return _activePalette; } }
 
 	[SerializeField] List<EnvironmentPalette> _environmentPaletteList = new List<EnvironmentPalette>();
 	public List<EnvironmentPalette> PalletteList { get { return _environmentPaletteList; } set { _environmentPaletteList = value; } }
 
+	[SerializeField] Material terrainMaterial;
+	[SerializeField] ParticleSystem groundSplatDecal;
+
 	void Awake () 
 	{
-		
+		//UpdatePalatte( _activePalette );
 	}
 
 	public override void Initialize ()
 	{
 
 		isInitialized = true;
+	}
+
+	void EditorUpdatePalatte( int newPalatteIndex )
+	{
+		if( _paletteIndex != _environmentPaletteList.FindIndex( x => x.title == _activePalette.title ) );
+		{
+			_activePalette = _environmentPaletteList[_paletteIndex];
+
+			//groundcolor
+			Shader.SetGlobalColor("_GroundColorPrimary", _activePalette.groundColorPrimary );
+			Shader.SetGlobalColor("_GroundColorSecondary", _activePalette.groundColorSecondary );
+			Shader.SetGlobalColor("_GroundColorTint", _activePalette.groundDecalTint );
+
+			if (groundSplatDecal != null )
+			{
+				ParticleSystem.MainModule groundDecalMain = groundSplatDecal.main;
+				groundDecalMain.startColor = new Color( _activePalette.groundColorSecondary.r * _activePalette.groundDecalTint.r, _activePalette.groundColorSecondary.g * _activePalette.groundDecalTint.g, _activePalette.groundColorSecondary.b * _activePalette.groundDecalTint.b, 1.0f );
+			}
+
+			//skybox
+			RenderSettings.fogColor = _activePalette.fogColor;
+			RenderSettings.skybox.SetColor( "_Color1", _activePalette.cloudRimColor );
+			RenderSettings.skybox.SetColor( "_Color2", _activePalette.skyColor );
+
+			//terrain
+			if ( terrainMaterial != null )
+			{
+				terrainMaterial.SetColor( "_Color", _activePalette.terrainColor );
+			}
+
+			if( ExecutePaletteChange != null )
+			{
+				ExecutePaletteChange( _activePalette );
+			}
+		}
+	}
+
+	/// <summary>
+	/// Applies the three part gradient to our Gradient Shader
+	/// </summary>
+	/// <param name="material">Material.</param>
+	/// <param name="gradient">Gradient.</param>
+	public static void ApplyThreePartGradient( Material material, Gradient gradient )
+	{
+		Debug.Assert( material != null && material.HasProperty("Top Color"), "Must be a gradient shader!" );
+
+		material.SetColor("Top Color", gradient.Evaluate(0f));
+		material.SetColor("Mid Color", gradient.Evaluate(.5f));
+		material.SetColor("Bot Color", gradient.Evaluate(1f));		
+	}
+
+	void OnValidate()
+	{		
+		if( _paletteIndex < 0 )
+		{
+			_paletteIndex = 0;
+		}
+		else if( _paletteIndex >= _environmentPaletteList.Count )
+		{
+			_paletteIndex = _environmentPaletteList.Count - 1;
+		}
+
+		EditorUpdatePalatte( _paletteIndex );
 	}
 }
