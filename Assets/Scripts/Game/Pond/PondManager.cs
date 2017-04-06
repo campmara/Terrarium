62a026e03c4d3759b65db5b5af137ec192a9698a
Murray Campbell <MurrayIRC@gmail.com>
2017-04-06 17:35:13 -0500

    Changelist
    - Fixed up all the ritual pond return stuff
    - separated the intro from the ritual
    - now we have an intro pan that gets triggered by the gamemanager and
    not that panel
    - ritual pan gets called for ritual and does a couple different things.

--- Assets/Scripts/Game/Pond/PondManager.cs
+++ Assets/Scripts/Game/Pond/PondManager.cs
@@ -45,6 +45,16 @@ public class PondManager : SingletonBehaviour<PondManager>
         }
     }
 
+    public void HandlePondReturn()
+    {
+        // Transport player to pond pop point.
+        // Tell the Camera to pan back to the pond.      
+        PlayerManager.instance.PutPlayerInPond();
+
+		GameManager.Instance.ChangeGameState( GameManager.GameState.POND_RETURN );
+        CameraManager.instance.ChangeCameraState( CameraManager.CameraState.POND_RETURNPAN );       
+    }
+
     public void PopPlayerFromPond()
     {
         GameManager.Instance.ChangeGameState( GameManager.GameState.POND_POP );
@@ -52,39 +62,13 @@ public class PondManager : SingletonBehaviour<PondManager>
         StartCoroutine( PopPlayerRoutine() );
     }
 
-    public void HandlePondReturn()
-    {
-        // Transport player to pond pop point.
-        // Tell the Camera to pan back to the pond.      
-        PlayerManager.instance.ReturnPlayerToPond(); 
-
-        //PlayerManager.instance.Player.ControlManager.SetActiveController<InactiveController>();
-
-        CameraManager.instance.ChangeCameraState( CameraManager.CameraState.POND_RETURNPAN );       
-    }
-
     private IEnumerator PopPlayerRoutine()
-    {        
-		//AudioManager.instance.PlayClipAtIndex( AudioManager.AudioControllerNames.PLAYER_TRANSITIONFX, 2 );
-
-		//PlayerManager.instance.Player.GetComponent<RollerController>().BecomeBall();
-
-        //Vector3 endPos = _pond.transform.forward * 5f;
-        //Tween jumpTween = PlayerManager.instance.Player.transform.DOJump( endPos, POP_HEIGHT, 1, POP_DURATION ).SetEase( Ease.Linear );
-        //yield return jumpTween.WaitForCompletion();
-
+    {
         Tween popTween = PlayerManager.instance.Player.transform.DOMoveY(PondTech.POND_MIN_Y, 0.75f).SetEase(Ease.OutBack);
+
         yield return popTween.WaitForCompletion();
-        
-        PlayerManager.instance.Player.ControlManager.SetActiveController<RollerController>();
 
         CameraManager.instance.ChangeCameraState( CameraManager.CameraState.FOLLOWPLAYER_FREE );
         GameManager.Instance.ChangeGameState( GameManager.GameState.MAIN );
-
-        // Zero out velocity.
-        PlayerManager.instance.Player.GetComponent<RollerController>().Velocity = 0f;
-        
-		//PlayerManager.instance.Player.GetComponent<RollerController>().BecomeWalker();
-        //PlayerManager.instance.Player.GetComponent<RollerController>().ChangeState( P_ControlState.WALKING );
     }
 }
