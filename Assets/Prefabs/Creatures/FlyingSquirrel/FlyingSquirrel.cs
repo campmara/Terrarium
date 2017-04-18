113f0dfcd1cae5304cb13fa5c58cb3e576586262
Murray Campbell <MurrayIRC@gmail.com>
2017-04-18 16:51:52 -0500

    Changelist
    - started work on the flying squirrel

---
+++ Assets/Prefabs/Creatures/FlyingSquirrel/FlyingSquirrel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingSquirrel : MonoBehaviour 
+{
+	/*
+	TODO:
+	- Make this.
+	- Subtle z rotation shifts on a sine wave, to make it look like it's very slightly lilting.
+	- starts at some point far away from the playspace, turns to face the center.
+	- moves forward very very slowly
+	- once it reaches the opposite end of where it started in terms of distance we can disable it.
+	- only appears once every 30 minutes??? something like that.
+	 */
+}
