3c9bcdc875cbc5721e21a73e220854b00f11a982
Thomas Newlands <thomas@newlands.cc>
2017-06-03 16:10:20 -0500

    base huppet stuff and gradient now also informs hardness of material

---
+++ Assets/Thomas/Plants/Class 3/Huppet/HuppetChild.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuppetChild : MonoBehaviour {
+
+    private Animator animator;
+    public Animator huppetFruit;
+    public float speed = .15f;
+
+    private void Start()
+    {
+        huppetFruit.enabled = false;
+        animator = GetComponent<Animator>();
+        SetRandomLayerWeight(animator);
+        animator.speed = speed;
+        transform.localScale *= Random.Range(2f, 3f);
+    }
+
+    private void Update()
+    {
+        if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f && !huppetFruit.enabled)
+        {
+            huppetFruit.enabled = true;
+        }
+    }
+
+    void SetRandomLayerWeight(Animator anim)
+    {
+        for (int i = 0; i < anim.layerCount; i++)
+        {
+            anim.SetLayerWeight(i, Random.Range(.05f, .25f));
+        }
+    }
+}
